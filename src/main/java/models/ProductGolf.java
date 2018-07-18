8c5993d9c4f51c617a6369c14c4d3de9114fbed2
PCUniverse28 <pcuniversersa@gmail.com>
2018-07-18 20:04:14 +0200

    test

--- src/main/java/models/ProductGolf.java
+++ src/main/java/models/ProductGolf.java
@@ -8,14 +8,18 @@ public class ProductGolf extends Product {
     private String productName;
     private String expiryDate;
     private String price;
+    private String commission;
+    private int carts;
 
-    public ProductGolf(String supplierName, String category, String province, String productName, String expiryDate, String price) {
+    public ProductGolf(String supplierName, String category, String province, String productName, String expiryDate, String price, String commission, int carts) {
         this.supplierName = supplierName;
         this.category = category;
         this.province = province;
         this.productName = productName;
         this.expiryDate = expiryDate;
         this.price = price;
+        this.commission = commission;
+        this.carts = carts;
     }
 
     public String getSupplierName() {
@@ -40,5 +44,22 @@ public class ProductGolf extends Product {
 
     public String getPrice() {
         return price;
+    }
+
+    public String getCommission() {
+        return commission;
+    }
+
+    public int getCarts() {
+        return carts;
+    }
+
+    @Override
+    public String toString(){
+        if(carts == 1) {
+            return productName + " incl carts - R" + price + " - Com:" + commission + "% - Expire: " + expiryDate;
+        } else {
+            return productName + " carts not included - R" + price + " - Com:" + commission + "% - Expire: " + expiryDate;
+        }
     }
 }
